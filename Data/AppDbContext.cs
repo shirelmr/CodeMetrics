070d2b48e79fdbecde47953230a1ca21da38d13c
Shirel Marino <shirelmr12@gmail.com>
2026-07-01 22:31:03 -0600

    add user to db

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -10,4 +10,12 @@ public class AppDbContext : DbContext
     }
 
     public DbSet<Repository> Repositories { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Email)
+            .IsUnique();
+    }
 }
